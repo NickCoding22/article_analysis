f06534d2365cda19a64639993203c83a45210534
Nick Angelici <nickangelici22@gmail.com>
2023-10-28 21:16:12 -0700

    Fixed Main.py

--- Backend/Main.py
+++ Backend/Main.py
@@ -5,7 +5,12 @@ import SentimentAnalysis
 # Returns the sentiment
 def main (url):
     sent = SentimentAnalysis.get_sentiment_from_article(Parser.parse_website(url))
+    final_sent = 0
+    for s in sent.keys():
+        if sent[s] > final_sent:
+            final_sent = sent[s]
+        
     summary = Backend.analyze_website_LLM(url)["main points"]
-    return [url, sent, summary]
+    return [url, final_sent, summary]
 
 # print(main("https://www.sfchronicle.com/bayarea/article/sf-city-college-revive-18417567.php"))
